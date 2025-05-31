9419b7d2b0529bd1cdd78b7b0c2937541d7de802
uzair514 <uzairch514@gmail.com>
2025-06-01 03:54:30 +0500

    Fixing Linting Errors

--- app.py
+++ app.py
@@ -13,13 +13,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 
 db.init_app(app)
 
-# @app.before_first_request
-# def create_tables():
-#     db.create_all()
 
 @app.route('/')
 def index():
     return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -30,12 +28,14 @@ def register():
         if existing_user:
             flash('Username already exists.')
             return redirect(url_for('register'))
-        new_user = User(username=username, password=generate_password_hash(password))
+        new_user = User(username=username,
+                        password=generate_password_hash(password))
         db.session.add(new_user)
         db.session.commit()
         flash('Registration successful.')
         return redirect(url_for('login'))
     return render_template('register.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -48,15 +48,18 @@ def login():
         flash('Invalid credentials.')
     return render_template('login.html')
 
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
 
 @app.before_request
 def create_tables_once():
     if not hasattr(app, 'db_initialized'):
         db.create_all()
         app.db_initialized = True
+
 
 if __name__ == '__main__':
     app.run(debug=True)
